705e0f8496fa17c52542251f19207dda2674e687
andurebel <andu.trica@gmail.com>
2021-06-18 12:19:40 +0300

    updated

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ import style from "./Button.module.css";
 const Button = () => {
   return (
     <div className={style.button}>
-      <span>Try it free 7 days</span> then $20/mo. thereafter
+      <span>Try it free 7 days</span> then $20/mo.
     </div>
   );
 };
